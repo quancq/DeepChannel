1253de520155783ddbd05024b0a98e319f0528a7
quancq <quancq.it@gmail.com>
2019-04-29 11:00:01 +0700

    Update

--- model/noisyChannel.py
+++ model/noisyChannel.py
@@ -4,22 +4,24 @@ from torch import nn
 from torch.nn import init, functional
 import numpy as np
 
+
 class ChannelModel(nn.Module):
 
     def __init__(self, **kwargs):
         super().__init__()
-        self.se_dim = kwargs['se_dim'] # dim of sentence embedding
+        self.se_dim = kwargs['se_dim']  # dim of sentence embedding
         self.p_producer = nn.Sequential(
-                nn.Dropout(kwargs['dropout']),
-                nn.Linear(self.se_dim * 3, 1024),
-                nn.ReLU(),
-                nn.Linear(1024, 256),
-                nn.ReLU(),
-                nn.Linear(256, 1),
-                nn.Sigmoid()
-                )
-        self.temperature = 1 # for annealing
+            nn.Dropout(kwargs['dropout']),
+            nn.Linear(self.se_dim * 3, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+        self.temperature = 1  # for annealing
         self.reset_parameters()
+
     def reset_parameters(self):
         for name, param in list(self.p_producer.named_parameters()):
             if 'bias' in name:
@@ -27,33 +29,30 @@ class ChannelModel(nn.Module):
             elif 'weight' in name:
                 init.kaiming_normal_(param)
 
-
-    def forward(self, D, S): # TODO batch with torch.bmm
+    def forward(self, D, S):  # TODO batch with torch.bmm
         '''
         To calculate P(D|S) given the sentence embeddings of D and S. 
         D: [n, Ds], where n is # sentences of document
         S: [m, Ds], where m is # sentences of summary
         '''
         n, m = D.size(0), S.size(0)
-        S_T = torch.transpose(S, 0, 1).contiguous() # [Ds, m]
+        S_T = torch.transpose(S, 0, 1).contiguous()  # [Ds, m]
         att_weight = functional.softmax(
-                torch.mm(D, S_T) / self.temperature,
-                dim=1
-                ) # [n, m]
-        att_S = torch.mm(att_weight, S) # [n, Ds]
-
+            torch.mm(D, S_T) / self.temperature,
+            dim=1
+        )  # [n, m]
+        att_S = torch.mm(att_weight, S)  # [n, Ds]
 
         ## --------------------------------------------------------------- ##
         d_s_feat = torch.cat([D, att_S, torch.mul(D, att_S)], dim=1)
-        prob_vector = self.p_producer(d_s_feat) # P(d|S) in each row
+        prob_vector = self.p_producer(d_s_feat)  # P(d|S) in each row
         ## --------------------------------------------------------------- ##
 
         # P(D|S) on the independent assumption. TODO: 0.5 trick not elegant;
         log_prob = torch.sum(torch.log(0.5 + prob_vector))
         addition = {
-                'prob_vector': prob_vector,
-                'att_weight': att_weight,
-                'att_S': att_S
-                }
-        return log_prob, addition 
-
+            'prob_vector': prob_vector,
+            'att_weight': att_weight,
+            'att_S': att_S
+        }
+        return log_prob, addition
